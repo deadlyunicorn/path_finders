7d2bdccc713b41500a3f1244012cb10a4c169a1f
deadlyunicorn <retroalex1008@gmail.com>
2023-11-03 02:51:25 +0200

    check only for location permission. Might change sensors package..

--- lib/src/sensors/sensors_controller.dart
+++ lib/src/sensors/sensors_controller.dart
@@ -7,30 +7,33 @@ class SensorsController with ChangeNotifier{
   SensorsController();
 
 
-  Future<Position> _determinePosition() async{
+  Future<bool> _hasLocationPermission() async{
     bool serviceEnabled;
     LocationPermission permission;
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
 
     if( !serviceEnabled ){
-      return Future.error('Location services are disabled');
+      return false;
+      // return Future.error('Location services are disabled');
     }
 
     permission = await Geolocator.checkPermission();
     if ( permission == LocationPermission.denied ){
       permission = await Geolocator.requestPermission();
       if ( permission == LocationPermission.denied){
-        return Future.error("Location permission denied");
+        return false;
+        // return Future.error("Location permission denied");
       }
     }
     else if ( permission == LocationPermission.deniedForever ){
-      return Future.error( "Please enable the location permission via your settings");
+      return false;
+      // return Future.error( "Please enable the location permission via your settings");
     }
 
-    return await Geolocator.getCurrentPosition();
+    return true;
   }
 
-  Future<Position> get determinePosition => _determinePosition();
+  Future<bool> get hasLocationPermission => _hasLocationPermission();
 }
 
