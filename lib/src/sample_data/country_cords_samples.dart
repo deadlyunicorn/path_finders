d79399e82c0ca3c9324e67e9a8d59c4754d2072d
deadlyunicorn <retroalex1008@gmail.com>
2023-11-05 17:53:00 +0200

    +sample data

---
+++ lib/src/sample_data/country_cords_samples.dart
@@ -0,0 +1,16 @@
+class SampleCoordinateData {
+
+  Coordinates chinaCords   =  Coordinates( 31.2183202, 120.2284013);
+  Coordinates mexicoCords  =  Coordinates( 19.3904678, -99.455446); 
+  Coordinates finlandCords =  Coordinates( 65.0679042, 25.58678 ); 
+  Coordinates sAfricaCords =  Coordinates( -33.925108, 18.5315826 ); 
+
+}
+
+class Coordinates {
+
+  double latitude;
+  double longitude;
+
+  Coordinates( this.latitude, this.longitude ); 
+}
