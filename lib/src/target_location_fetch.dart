22d7690b15a93c32239e7d8bfb2d815eab420320
deadlyunicorn <retroalex1008@gmail.com>
2023-11-22 21:54:08 +0200

    target location services optimizations

--- lib/src/target_location_fetch.dart
+++ lib/src/target_location_fetch.dart
@@ -3,22 +3,32 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'package:path_finders/src/types/coordinates.dart';
-import 'package:path_finders/src/types/targetDetails.dart';
+import 'package:path_finders/src/types/target_details.dart';
 
 class TargetLocationServices {
 
   
-  static Future<TargetDetails?> getTargetDetails( String targetId ) async{
+  static Future<TargetDetails?> getTargetDetails( String targetIdWithDash ) async{
     
     final DateTime? updatedAt;
     final String? errorMessage;
+    final String targetId = targetIdWithDash.replaceAll('-', '');
     
     final targetInfoBody = await http.get( Uri.parse("https://path-finders-backend.vercel.app/api/users/$targetId" ))
       .then( ( response ) => jsonDecode( response.body )  );
 
+    print( targetInfoBody );
 
 
-    if ( targetInfoBody["data"] != null ){
+    if( targetInfoBody["error"] != null ){
+
+        errorMessage = targetInfoBody["error"]["message"];
+
+        return TargetDetails(
+          errorMessage: errorMessage,
+        );
+    }
+    else if ( targetInfoBody["data"] != null ){
 
       updatedAt = DateTime.tryParse( targetInfoBody["data"]["updatedAt"].toString() );
 
@@ -34,15 +44,9 @@ class TargetLocationServices {
           updatedAt: updatedAt ?? DateTime.now()
         );
       }
-      else if( targetInfoBody["error"] != null ){
-        errorMessage = targetInfoBody["error"]["message"];
-
-        return TargetDetails(
-          errorMessage: errorMessage,
-          updatedAt: updatedAt ?? DateTime.now(),
-        );
-      }
+      
     }
+    
     
 
     return null;
@@ -59,7 +63,12 @@ class TargetLocationServices {
       fetchCount ++   ;
       yield fetchCount;
       
-      await Future.delayed( const Duration( minutes: 2 ) );
+      await Future.delayed( 
+        const Duration(
+           minutes: 2,
+          //  seconds: 5
+        ) 
+      );
     }
   }
 
