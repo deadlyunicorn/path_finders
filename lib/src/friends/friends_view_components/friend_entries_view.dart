53905126102094d171b3730ba84acf09224189db
deadlyunicorn <retroalex1008@gmail.com>
2023-11-23 13:59:54 +0200

    optimized code: fetches only once during build

--- lib/src/friends/friends_view_components/friend_entries_view.dart
+++ lib/src/friends/friends_view_components/friend_entries_view.dart
@@ -40,99 +40,106 @@ class FriendEntriesView extends StatelessWidget{
                 //is that on entry addition/removal
                 //we don't need to return a loading screen.
                 //We still keep our old entry list loaded.
-                
                 listingsProvider.initializeTargetEntries( snapshotData );
+                
+
+                return StreamBuilder(
+                  stream: TargetLocationServices.targetLocationIntervalStream(), 
+                  builder: ( context, streamerSnapshot ){
+
+                    if ( streamerSnapshot.connectionState == ConnectionState.active ){
+                      return ListView.builder(
+                        itemCount:  listingsProvider.targetEntries.length,
+                        itemBuilder: (context, index) {
+
+                          //using .read() instead of .watch()
+                          //to not rebuild/refetch when changing targets.
+                          final targetProvider = context.read<TargetProvider>();
+                          
+                          final targetId = listingsProvider.targetEntries.elementAt(index);
+                          
+                          if ( 
+                            targetId == targetProvider.targetName
+                          ){
+                            print( "ok");
+                            // targetProvider.setTargetLocation( snapshotData.getCoordinates()! );
+                          }
+
+
+
+
+                          return FutureBuilder(
+                            future: TargetLocationServices.getTargetDetails( targetId ), 
+                            builder: ( context, snapshot ) {
+
+                              final snapshotData = snapshot.data; 
+                              Widget leadingIcon;
+
+                              switch ( snapshot.connectionState ){
+
+                                case ConnectionState.waiting:
+                                  leadingIcon = const CircularProgressIndicator ( strokeWidth: 0.1,);
+                                  break;
+
+                                case ConnectionState.done:
+                                  if ( snapshot.hasData && snapshotData != null ){
+
+                                    if ( snapshotData.hasErrorMessage() ){
+                                      leadingIcon = const Icon ( Icons.error, color: Colors.red );
+                                    }
+                                    else{
+                                      leadingIcon = const Icon( Icons.done, color: Colors.green, );
+                                    }
+                                    break;
+
+                                  }
+                                  else{
+                                    leadingIcon = const Icon( Icons.warning, color: Colors.yellow );
+                                    break;
+                                  }
+
+                                default:
+                                  leadingIcon = const Icon( Icons.warning, color: Colors.yellow );
+                                  break;
+
+                              }
+
+                              return ListTile(
+                                title: Text( targetId ), 
+                                leading: SizedBox( width: 32, height: 32, child: leadingIcon) ,
+                                trailing:  ( snapshotData != null && snapshotData.hasErrorMessage() )
+                                  ? Text( snapshotData.getErrorMessage()! )
+                                  : const SizedBox.shrink(),
+                                onTap: () {
+                                  if ( snapshotData != null && !snapshotData.hasErrorMessage() ){
+
+                                    targetProvider.setTargetName( targetId ) ;
+                                    targetProvider.setTargetLocation( snapshotData.getCoordinates()! ) ;
+
+                                  }
+                                },
+                                onLongPress: () {
+                                  listingsProvider.removeTargetEntry( targetId );
+
+                                },
+                              );
+
+                            }
+                          );
+                        },
+                      );
+                    }
+                    else{
+                      return const CircularProgressIndicator();
+                    }
+                  } 
+                ); 
 
               }
 
-              return StreamBuilder(
-                stream: TargetLocationServices.targetLocationIntervalStream(), 
-                builder: ( context, index){
-
-
-                  return ListView.builder(
-                    itemCount:  listingsProvider.targetEntries.length,
-                    itemBuilder: (context, index) {
-
-                      //using .read() instead of .watch()
-                      //to not rebuild/refetch when changing targets.
-                      final targetProvider = context.read<TargetProvider>();
-                      
-                      final targetId = listingsProvider.targetEntries.elementAt(index);
-                      
-                      if ( 
-                        targetId == targetProvider.targetName
-                      ){
-                        print( "ok");
-                        // targetProvider.setTargetLocation( snapshotData.getCoordinates()! );
-                      }
-
-
-
-
-                      return FutureBuilder(
-                        future: TargetLocationServices.getTargetDetails( targetId ), 
-                        builder: ( context, snapshot ) {
-
-                          final snapshotData = snapshot.data; 
-                          Widget leadingIcon;
-
-                          switch ( snapshot.connectionState ){
-
-                            case ConnectionState.waiting:
-                              leadingIcon = const CircularProgressIndicator ( strokeWidth: 0.1,);
-                              break;
-
-                            case ConnectionState.done:
-                              if ( snapshot.hasData && snapshotData != null ){
-
-                                if ( snapshotData.hasErrorMessage() ){
-                                  leadingIcon = const Icon ( Icons.error, color: Colors.red );
-                                }
-                                else{
-                                  leadingIcon = const Icon( Icons.done, color: Colors.green, );
-                                }
-                                break;
-
-                              }
-                              else{
-                                leadingIcon = const Icon( Icons.warning, color: Colors.yellow );
-                                break;
-                              }
-
-                            default:
-                              leadingIcon = const Icon( Icons.warning, color: Colors.yellow );
-                              break;
-
-                          }
-
-                          return ListTile(
-                            title: Text( targetId ), 
-                            leading: SizedBox( width: 32, height: 32, child: leadingIcon) ,
-                            trailing:  ( snapshotData != null && snapshotData.hasErrorMessage() )
-                              ? Text( snapshotData.getErrorMessage()! )
-                              : const SizedBox.shrink(),
-                            onTap: () {
-                              if ( snapshotData != null && !snapshotData.hasErrorMessage() ){
-
-                                targetProvider.setTargetName( targetId ) ;
-                                targetProvider.setTargetLocation( snapshotData.getCoordinates()! ) ;
-
-                              }
-                            },
-                            onLongPress: () {
-                              listingsProvider.removeTargetEntry( targetId );
-
-                            },
-                          );
-
-                        }
-                      );
-                    },
-                  );
-                  
-                } 
-              ); 
+              else { 
+                return const CircularProgressIndicator();
+              }
             },
           )
         ),
