47d28f128b07a8e187832f72c741ca4c87027e29
deadlyunicorn <retroalex1008@gmail.com>
2023-11-25 04:09:25 +0200

    +ListHeader custom Widget

--- lib/src/friends/friends_view.dart
+++ lib/src/friends/friends_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:path_finders/src/friends/entry_insertion_buttons.dart';
 import 'package:path_finders/src/friends/friends_view_components/live_entries_view.dart';
 import 'package:path_finders/src/friends/friends_view_components/static_entries_view.dart';
-import 'package:path_finders/src/friends/id_formatter.dart';
-import 'package:path_finders/src/providers/target_listings_provider.dart';
 import 'package:path_finders/src/providers/target_provider.dart';
 import 'package:path_finders/src/types/coordinates.dart';
 import 'package:provider/provider.dart';
@@ -35,8 +33,11 @@ class _FriendsViewState extends State<FriendsView> {
       Column(
         children: [
           Text("Currently ${context.watch<TargetProvider>().targetName} is selected."),
+          const ListHeader(text: "Static Entries"),
           StaticEntriesView( listItems: sampleData ),
+          const ListHeader(text: "Live Entries"),
           const LiveEntriesView(),
+          const EntryInsertionButtons(),
           Consumer<TargetListingsProvider>(
             builder: (context, listingsProvider, child) 
             =>Expanded(
@@ -93,4 +94,23 @@ class _FriendsViewState extends State<FriendsView> {
       )
     );
   }
+}
+
+class ListHeader extends StatelessWidget{ 
+
+  final String text;
+  const ListHeader( { required this.text, super.key } );
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      padding: const EdgeInsets.all( 8 ),
+      child: Text( 
+        text, 
+        textScaler: const TextScaler.linear( 1.8 )
+      ),
+    );
+  }  
 }
