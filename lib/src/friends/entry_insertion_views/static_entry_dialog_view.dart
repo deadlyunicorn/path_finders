131c0e2364c252dba3caf6b9e99689bacce0cf3b
deadlyunicorn <retroalex1008@gmail.com>
2023-11-28 03:17:32 +0200

    +Show some errors on Wrong Coordinates typing

--- lib/src/friends/entry_insertion_views/static_entry_dialog_view.dart
+++ lib/src/friends/entry_insertion_views/static_entry_dialog_view.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:path_finders/src/custom/snackbar_custom.dart';
 
 import 'package:path_finders/src/providers/targets_with_coordinates_provider.dart';
 import 'package:path_finders/src/types/coordinates.dart';
@@ -16,9 +17,9 @@ class StaticEntryDialog extends StatefulWidget{
 
 class _StaticEntryDialogState extends State<StaticEntryDialog> {
 
-  String targetName="";
-  double? latitude;
-  double? longitude;
+  String _targetName="";
+  double? _latitude;
+  double? _longitude;
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +36,7 @@ class _StaticEntryDialogState extends State<StaticEntryDialog> {
             keyboardType: TextInputType.name,
             onChanged: (value) {
               setState(() {
-                targetName = value;
+                _targetName = value;
               });
             },
             decoration: const InputDecoration(
@@ -51,11 +52,34 @@ class _StaticEntryDialogState extends State<StaticEntryDialog> {
           TextField(
             keyboardType: TextInputType.number,
             onChanged: (value){
-              latitude = double.tryParse(value) ;
+              final latitude = double.tryParse(value);
+              if ( latitude == null ){
+                ScaffoldMessenger
+                .of(context)
+                .showSnackBar(
+                  CustomSnackBar(
+                    duration: const Duration( milliseconds: 500),
+                    bgColor: Colors.red.shade700.withAlpha( 100 ),
+                    textContent: "Invalid latitude (Not a valid number)", 
+                    context: context
+                  )
+                );
+
+              }
+              else if (  latitude > 90 || latitude < -90  ){
+                ScaffoldMessenger
+                .of(context)
+                .showSnackBar(
+                  CustomSnackBar(
+                    duration: const Duration( milliseconds: 500),
+                    bgColor: Colors.red.shade700.withAlpha( 100 ),
+                    textContent: "Invalid latitude (Not a valid value)", 
+                    context: context
+                  )
+                );
+              }
+              _latitude = latitude;
             },
-            inputFormatters: [
-              // 
-            ],
             decoration: const InputDecoration(
               labelText: "Latitude",
               hintText: "A cool number like 11.04124",
@@ -70,7 +94,36 @@ class _StaticEntryDialogState extends State<StaticEntryDialog> {
           TextField(
             keyboardType: TextInputType.number,
             onChanged: (value){
-              longitude = double.tryParse(value) ;
+
+              final longitude = double.tryParse(value);
+              if ( longitude == null ){
+                ScaffoldMessenger
+                .of(context)
+                .showSnackBar(
+                  CustomSnackBar(
+                    duration: const Duration( seconds: 1),
+                    bgColor: Colors.red.shade700.withAlpha( 100 ),
+                    textContent: "Invalid longitude (Not a valid number)", 
+                    context: context
+                  )
+                );
+
+              }
+              else if (  longitude > 180 || longitude < -180  ){
+                ScaffoldMessenger
+                .of(context)
+                .showSnackBar(
+                  CustomSnackBar(
+                    duration: const Duration( seconds: 1),
+                    bgColor: Colors.red.shade700.withAlpha( 100 ),
+                    textContent: "Invalid longitude (Not a valid value)", 
+                    context: context
+                  )
+                );
+              }
+              _longitude = longitude;
+
+              
             },
             decoration: const InputDecoration(
               labelText: "Longitude",
@@ -85,17 +138,25 @@ class _StaticEntryDialogState extends State<StaticEntryDialog> {
         }, child: const Text("Cancel")),
         TextButton(
             onPressed: () async{
-              ( targetName.isNotEmpty 
+
+              final longitude = _longitude;
+              final latitude  = _latitude;
+
+              if( _targetName.isNotEmpty 
                && latitude != null
                && longitude != null 
-              ) 
-              ?(() async{
-                await listingsProvider.addTargetWithCoordinatesEntry( targetName, Coordinates(latitude!, longitude!) );
-                if ( context.mounted ){
-                  Navigator.pop(context, "Submit");
-                } 
-              })()
-              :null;
+               && ( latitude < 90 && latitude > -90 )
+               && ( longitude < 180 && longitude > -180  )){
+
+                (() async{
+                
+                  await listingsProvider.addTargetWithCoordinatesEntry( _targetName, Coordinates(_latitude!, _longitude!) );
+                  if ( context.mounted ){
+                    Navigator.pop(context, "Submit");
+                  } 
+                })();
+
+              }
             }, 
             child: const Text("Submit")
         )
