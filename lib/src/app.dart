86c737d29589f51586ed783b01d037281d4f981c
deadlyunicorn <petrache.dev@gmail.com>
2023-12-19 18:18:38 +0200

    refactoring

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,25 +1,17 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:path_finders/notifications/notification_controller.dart';
-import 'package:path_finders/src/custom/isLandscape.dart';
-import 'package:path_finders/src/settings/button.dart';
-import 'package:path_finders/src/storage_services.dart';
-import 'package:path_finders/src/using_the_app_dialog.dart';
+import 'package:path_finders/src/page_selector.dart';
+import 'settings/settings_controller.dart';
 import 'package:provider/provider.dart';
+
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'settings/settings_controller.dart';
-
-import 'package:path_finders/src/friends/friends_view.dart';
-import 'package:path_finders/src/location_services_checker_view.dart';
 import 'package:path_finders/src/navigation_bar/navigation_bar.dart';
-import 'package:path_finders/src/profile/profile_view.dart';
-import 'package:path_finders/src/providers/location_services_provider.dart';
-import 'package:path_finders/src/providers/target_with_id_listings_provider.dart';
-import 'package:path_finders/src/providers/target_provider.dart';
-import 'package:path_finders/src/providers/targets_with_coordinates_provider.dart';
-import 'package:path_finders/src/tracker/tracker_view.dart';
+import 'package:path_finders/notifications/notification_controller.dart';
+import 'package:path_finders/src/custom/isLandscape.dart';
+import 'package:path_finders/src/settings/button.dart';
+
 
 /// The Widget that configures your application.
 class MyApp extends StatefulWidget {
@@ -124,79 +116,4 @@ class _MyAppState extends State<MyApp> {
 
 
 
-class PageSelector extends StatelessWidget{
-  
-  const PageSelector({ super.key });
 
-  @override
-  Widget build(BuildContext context) {
-
-
-    //Future that reads if it has been displayed.
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) { 
-
-      IntroFinishFile
-        .introHasBeenFinished()
-        .then(
-          ( isTrue ) {
-            if ( isTrue ){
-
-            }
-            else{
-
-              showDialog(
-                barrierDismissible: false,
-                context: context, 
-                builder: ( context ) => UsingTheAppDialog()
-              );
-
-            }
-          }
-        );
-      
-    });
-
-
-
-    var appState = context.watch<CurrentPageState>();
-    int selectedIndex = appState.currentPageIndex;
-
-    return MultiProvider(
-      providers: [ 
-        ChangeNotifierProvider(
-          create: (context) => TargetWithIdListingsProvider()
-        ),
-        ChangeNotifierProvider(
-          create: (context) => TargetProvider()
-        ),
-        ChangeNotifierProvider(
-          create: (_) => LocationServicesProvider() 
-        ),
-        ChangeNotifierProvider(
-          create: (_) => TargetWithCoordinatesListingsProvider()
-        )
-      ],
-      child: IndexedStack(
-        index: selectedIndex,
-        children: const [
-          FriendsView(),
-          LocationServicesCheckerView(child: TrackerView()),
-          LocationServicesCheckerView(child: ProfileView()) 
-        ],
-      )
-    );
-  }
-
-}
-
-class CurrentPageState extends ChangeNotifier {
-
-  int _currentPageIndex = 1;
-
-  int get currentPageIndex => _currentPageIndex;
-
-  void setCurrentPageIndex( int value){
-    _currentPageIndex = value;
-    notifyListeners();
-  }
-}
