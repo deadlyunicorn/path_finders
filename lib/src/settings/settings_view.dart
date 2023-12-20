e71c82f7d0c1cb874446ebcbd61212ba3cc7ee79
deadlyunicorn <petrache.dev@gmail.com>
2023-12-20 15:10:58 +0200

    added url launcher

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:path_finders/src/custom/snackbar_custom.dart';
+import 'package:path_finders/src/custom/styles.dart';
+import 'package:path_finders/src/settings/bug_report_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 import 'settings_controller.dart';
@@ -31,57 +35,97 @@ class SettingsView extends StatelessWidget {
         // SettingsController is updated, which rebuilds the MaterialApp.
         child: Column(
 
-          children: [
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-            Row(
+          children: [
+            
+            Column(
               children: [
-                Text( "${appLocalizations.settings_theme}: ", style: Theme.of(context).textTheme.bodyLarge ),
-                DropdownButton<ThemeMode>(
-                  // Read the selected themeMode from the controller
-                  value: settingsController.themeMode,
-                  // Call the updateThemeMode method any time the user selects a theme.
-                  onChanged: settingsController.updateThemeMode,
-                  items: [
-                    DropdownMenuItem(
-                      value: ThemeMode.system,
-                      child: Text( appLocalizations.settings_systemTheme),
+                Row(
+                  children: [
+                    Text( "${appLocalizations.settings_theme}: ", style: Theme.of(context).textTheme.bodyLarge ),
+                    DropdownButton<ThemeMode>(
+                      // Read the selected themeMode from the controller
+                      value: settingsController.themeMode,
+                      // Call the updateThemeMode method any time the user selects a theme.
+                      onChanged: settingsController.updateThemeMode,
+                      items: [
+                        DropdownMenuItem(
+                          value: ThemeMode.system,
+                          child: Text( appLocalizations.settings_systemTheme),
+                        ),
+                        DropdownMenuItem(
+                          value: ThemeMode.light,
+                          child: Text( appLocalizations.settings_lightMode ),
+                        ),
+                        DropdownMenuItem(
+                          value: ThemeMode.dark,
+                          child: Text( appLocalizations.settings_darkMode),
+                        )
+                      ],
                     ),
-                    DropdownMenuItem(
-                      value: ThemeMode.light,
-                      child: Text( appLocalizations.settings_lightMode ),
-                    ),
-                    DropdownMenuItem(
-                      value: ThemeMode.dark,
-                      child: Text( appLocalizations.settings_darkMode),
-                    )
                   ],
                 ),
+                Row(
+                  children: [
+                    Text( "${appLocalizations.settings_language}: ", style: Theme.of(context).textTheme.bodyLarge ),
+                    DropdownButton(
+                      value: settingsController.locale,
+                      items: [
+                        DropdownMenuItem(
+                          child: Text( "🇺🇸 English - ${appLocalizations.lang_english}" ),
+                          value: "en",
+                        ),
+                        DropdownMenuItem(
+                          child: Text( "🇬🇷 Greek - ${appLocalizations.lang_greek}" ),
+                          value: "el",
+                        ),
+                      ], 
+                      onChanged: (value) {
+                        if ( value != null ){
+                          settingsController.updateLocaleSetting( value );
+                        }
+                      },
+                    )
+                  ],
+                )
+
               ],
             ),
-            Row(
+            Column(
               children: [
-                Text( "${appLocalizations.settings_language}: ", style: Theme.of(context).textTheme.bodyLarge ),
-                DropdownButton(
-                  value: settingsController.locale,
-                  items: [
-                    DropdownMenuItem(
-                      child: Text( "🇺🇸 English - ${appLocalizations.lang_english}" ),
-                      value: "en",
-                    ),
-                    DropdownMenuItem(
-                      child: Text( "🇬🇷 Greek - ${appLocalizations.lang_greek}" ),
-                      value: "el",
-                    ),
-                  ], 
-                  onChanged: (value) {
-                    if ( value != null ){
-                      settingsController.updateLocaleSetting( value );
+                TextButton(
+                  style: squaredButtonStyle,
+                  onPressed: ()async{
+                    
+                    ScaffoldMessenger.of(context)
+                    .showSnackBar( CustomSnackBar(textContent: "Not yet Implmented - Contact Dev", context: context));
+                    
+                    final appId = ""; //To be implemented
+
+                    try{
+
+                       await launchUrl( 
+                        Uri.parse("market://details?id=$appId"),
+                        mode: LaunchMode.externalApplication 
+                       );
+
                     }
-                  },
-                )
+                    catch( error ){ //no playstore installed?!
+                      await launchUrl( 
+                        Uri.parse("https://play.google.com/store/apps/details?id=$appId"),
+                        mode: LaunchMode.externalApplication 
+                       );
+
+                    }
+                   
+                  }, 
+                  child: Text( appLocalizations.reviewText )
+                ),
+                
+
               ],
             )
-
           ],
         ) 
         
