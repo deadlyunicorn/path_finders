6aad9f1ba7fc95e1e8a792dcf96bca213df878fd
deadlyunicorn <petrache.dev@gmail.com>
2023-12-28 02:18:12 +0200

    removed old notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:path_finders/notifications/notification_controller.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -8,20 +7,13 @@ import 'src/settings/settings_service.dart';
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationController.initializeLocalNotifications();
-  await NotificationController.initializeIsolateReceivePort();
+
 
   final settingsController = SettingsController(SettingsService());
 
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-
-  
-
   // await NotificationAbstractions.intialization();
-
-  
-
   runApp(MyApp(settingsController: settingsController));
 }
